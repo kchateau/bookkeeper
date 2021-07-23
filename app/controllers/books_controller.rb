4521a81ffd6aa699dc48c18deed6324f477854cd
Kendra Chateau <kendra@super.gd>
2021-07-23 08:50:49 -0700

    Create UserRead when Book is created by a user

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,12 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
   def new
     @book = Book.new
   end
 
   def create
     @book = Book.create(book_params)
+    create_user_read(@book)
     redirect_to root_path
   end
 
@@ -12,5 +14,9 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :description, :isbn, :genre_ids => [])
+  end
+
+  def create_user_read(book)
+    UserRead.create(user: current_user, book: book, rating: 5)
   end
 end
