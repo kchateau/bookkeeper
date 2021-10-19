f26ee9b9dfdc2d23e8d45c8cd67d373c73e06960
Kendra Chateau <kendra@super.gd>
2021-10-19 08:28:50 -0700

    Add ImageLink field to Books
    I added this field to the Books model so that I can save and display the Books
    cover image.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_232900) do
+ActiveRecord::Schema.define(version: 2021_10_19_152802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_07_29_232900) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "genre_id"
+    t.string "image_link"
     t.index ["author_id"], name: "index_books_on_author_id"
     t.index ["genre_id"], name: "index_books_on_genre_id"
   end
